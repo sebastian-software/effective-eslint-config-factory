5d673ac341ab0b63c04e670a4237d13a0c5d1c76
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-20 21:22:36 +0100

    Worked on eslint/react recommended loading

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
+import pkgDir from 'pkg-dir';
+import {join} from "path"
+import { configs } from "eslint-plugin-react"
+
 interface CliOptions {
   nodejs: boolean
   react: boolean
   typescript: boolean
 }
 
-export function main(flags: CliOptions) {
+export async function main(flags: CliOptions) {
   console.log("Effective ESLint...", flags)
+
+  const eslintRecommended = await getESLintRecommended()
+  console.log("ESLint:",eslintRecommended)
+
+  const reactRecommended = await getReactRecommended()
+  console.log("React:", reactRecommended)
+}
+
+async function getESLintRecommended() {
+  const root = await pkgDir(require.resolve("eslint"))
+  if (!root) {
+    throw new Error("Installation Issue: ESLint package was not found!")
+  }
+  const recommendedPath = join(root, "conf/eslint-recommended")
+  const { rules } = await import(recommendedPath)
+  return {
+    rules
+  }
+}
+
+async function getReactRecommended() {
+  const react = await import("eslint-plugin-react")
+  const { rules, ...config } = react.configs.recommended
+  return {
+    rules,
+    config
+  }
 }
