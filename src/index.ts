c3eb1e70594bad9a212722e0e3ba006f3fe4526c
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-20 17:57:43 +0100

    Continue development

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import meow from 'meow';
+import { main } from './command';
+
+const cli = meow(`
+	Usage
+	  $ foo <input>
+
+	Options
+	  --rainbow, -r  Include a rainbow
+
+	Examples
+	  $ foo unicorns --rainbow
+	  🌈 unicorns 🌈
+`, {
+	importMeta: import.meta,
+	flags: {
+		rainbow: {
+			type: 'boolean',
+			alias: 'r'
+		}
+	}
+});
+/*
+{
+	input: ['unicorns'],
+	flags: {rainbow: true},
+	...
+}
+*/
+
+command(cli.input[0], cli.flags);
