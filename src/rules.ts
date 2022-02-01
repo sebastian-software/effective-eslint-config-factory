cc47b45cc20b5bb3146d1e712a4399e39cade0fb
Sebastian Werner <s.werner@sebastian-software.de>
2022-02-01 22:57:49 +0100

    Worked on new rule resolver

--- src/rules.ts
+++ src/rules.ts
@@ -1,6 +1,52 @@
 import { SourcePriorityTable } from "./types"
 
-export const ruleBasedSourcePriority: SourcePriorityTable = {}
+export const ruleBasedSourcePriority: SourcePriorityTable = {
+  "camelcasex": "xo-typescript",
+  "class-methods-use-this": "off", // too limiting, same in most presets
+  "complexity": "kentdodds", // configured to 20, Kent only
+  "constructor-super": "xo", // also: eslint, xo
+  "default-case": "xo", // also: kentdodds
+  "capitalized-comments": "off", // too limiting,
+  "eqeqeq": "cra", // smart mode enabled
+  "func-name-matching": "kentdodds", // enabled but without property descriptor (rarely used)
+  "func-names": "kentdodds", // prefer better debug-ability
+  "getter-return": "eslint", // for what a getter if not for return, same in most presets
+  "grouped-accessor-pairs": "xo", // get before set makes sense, rarely used rule
+  "max-depth": "kentdodds", // configured to 4, Kent only
+  "max-nested-callbacks": "xo", // in Kent and XO, stricter in XO
+  "max-params": "xo", // in Kent and XO, stricter in XO
+  "max-statements-per-line": "xo", // in Kent and XO, Kent defines a max=1 which is already the default
+  "new-cap": "kentdodds", // in Kent and XO, XO defines options which are already the default
+  "no-async-promise-executor": "eslint", // Typically an oversight. Also in XO.
+  "no-cond-assign": "eslint", // as in most presets, except Kent
+  "no-const-assign": "eslint", // as in most presets, except Kent
+  "no-dupe-args": "eslint", // as in most presets, except Kent
+  "no-dupe-keys": "eslint", // as in most presets, except Kentx^
+  "no-else-return": "off", // only in XO, too strict
+  "no-empty": "eslint", // as in most presets
+  "no-eq-null": "off", // only in XO, too strict, better use eqeqeq rule
+  "no-extra-boolean-cast": "eslint", // as in most presets, except Kent
+  "no-func-assign": "eslint", // as in most presets, except Kent
+  "no-implicit-coercion": "xo", // only in XO, but a good one to improve code accessibility
+  "no-import-assign": "eslint", // do not work in ESM anyway and bad style
+  "no-labels": "xo", // same for Kent, without exceptions for loops (enabled in CRA... for whatever reason)
+  "no-misleading-character-class": "eslint", // as in most presets
+  "no-new-symbol": "eslint", // as in most presets
+  "no-obj-calls": "eslint", // as in most presets
+  "no-promise-executor-return": "off", // unconfigured in most presets except XO
+  "no-prototype-builtins": "eslint", // technique from the past, better omit
+  "no-restricted-globals": "cra", // best richest set from CRA, others only enable it, without config
+  "no-restricted-properties": "cra", // best richest set from CRA, others only enable it, without config
+  "no-return-assign" : "kentdodds", // same in XO, otherwise unconfigured
+  "no-return-await": "off", // setting seems controversial with no clear benefits when enabling globally
+  "no-self-assign": "eslint", // effectively same value in all presets
+  "no-setter-return": "eslint", // same as XO
+  "no-this-before-super": "eslint", // as in most presets
+  "no-undef": "xo", // typeof is enabled in XO only, which is a valid use case
+  "no-underscore-dangle": "off", // rarely used
+  "no-unreachable": "eslint", // as in most presets
+
+}
 
 export const ruleBasedSourcePriorityOld: SourcePriorityTable = {
   // Additional "except-parens" was used in some, but that's the default anyway
