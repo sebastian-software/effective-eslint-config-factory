6ce33b7f4e606743d57b6b7ef9cffd6c6deab2ed
Sebastian Werner <s.werner@sebastian-software.de>
2022-02-02 23:52:38 +0100

    Fixed final conflicts + updated snapshots

--- src/rules.ts
+++ src/rules.ts
@@ -35,10 +35,67 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
   "strict": "kentdodds", // source format is ESM in most code bases, still useful as auto-detected
   "valid-typeof": "eslint", // common in most presets
 
-  // "@typescript-eslint/ban-types": "xo-typescript", // best collection of types to ban is in XO
-  // "@typescript-eslint/ban-ts-comment": "xo-typescript", // XO allows exceptions which are a good idea
-  // "@typescript-eslint/array-type": "xo-typescript", // Prefer simple arrays as in XO
+  "@typescript-eslint/array-type": "xo-typescript", // Prefer simple arrays as in XO
+  "@typescript-eslint/ban-ts-comment": "xo-typescript", // XO allows exceptions which are a good idea
+  "@typescript-eslint/ban-types": "xo-typescript", // best collection of types to ban is in XO
+  "@typescript-eslint/consistent-type-assertions": "xo-typescript", // enforcing 'as' type is standard nowadays
+  "@typescript-eslint/no-empty-interface": "xo-typescript",
+  "@typescript-eslint/no-extraneous-class": "xo-typescript",
+  "@typescript-eslint/no-floating-promises": "xo-typescript",
+  "@typescript-eslint/no-misused-promises": "xo-typescript",
+  "@typescript-eslint/no-this-alias": "xo-typescript",
+  "@typescript-eslint/no-throw-literal": "xo-typescript",
+  "@typescript-eslint/no-unused-vars": "xo-typescript",
+  "@typescript-eslint/no-use-before-define": "off", // not that relevant in todays world
+  "@typescript-eslint/restrict-plus-operands": "xo-typescript",
+  "@typescript-eslint/restrict-template-expressions": "xo-typescript",
+  "@typescript-eslint/triple-slash-reference": "ts", // effectively identical in all presets
 
+  "import/named": "import", // follow the plugin hint for usage in TS projects
+  "import/namespace": "xo-typescript", // should be solved by TS compiler already
+
+  "jsx-a11y/alt-text": "jsx", // effectively identical in all presets
+  "jsx-a11y/anchor-has-content": "jsx", // effectively identical in all presets
+  "jsx-a11y/anchor-is-valid": "cra", // slightly adjusted version in CRA seems good
+  "jsx-a11y/aria-role": "cra", // slightly adjusted version in CRA seems good
+  "jsx-a11y/control-has-associated-label": "off", // disabled in jsx, too, tricky with custom components
+  "jsx-a11y/heading-has-content": "jsx", // effectively identical in all presets
+  "jsx-a11y/interactive-supports-focus": "jsx", // follow plugin author recommendation
+  "jsx-a11y/label-has-associated-control": "off", // disabled in cra, too, tricky with custom components
+  "jsx-a11y/media-has-caption": "jsx", // effectively identical in all presets
+  "jsx-a11y/no-autofocus": "off", // disabled in cra, too. Not that relevant I guess.
+  "jsx-a11y/no-distracting-elements": "jsx", // effectively identical in all presets
+  "jsx-a11y/no-interactive-element-to-noninteractive-role": "jsx", // follow plugin author recommendation
+  "jsx-a11y/no-noninteractive-element-interactions": "jsx", // follow plugin author recommendation
+  "jsx-a11y/no-noninteractive-element-to-interactive-role": "jsx", // follow plugin author recommendation
+  "jsx-a11y/no-noninteractive-tabindex": "jsx", // follow plugin author recommendation
+  "jsx-a11y/no-static-element-interactions": "jsx", // follow plugin author recommendation
+
+  "react/button-has-type": "xo-react", // effectively identical in all presets
+  "react/function-component-definition": "airbnb-react", // old school: prefer real inspectable functions
+  "react/default-props-match-prop-types": "off", // TS mode: Do not care about prop-types
+  "react/jsx-boolean-value": "xo-react", // effectively identical in all presets: the default value of this option is "never"
+  "react/jsx-curly-brace-presence": "xo-react", // reduce clutter, formatting related
+  "react/jsx-filename-extension": "kentdodds", // respect .ts/.tsx
+  "react/jsx-key": "xo-react", // again a fine tuned variant over the default ones in other presets
+  "react/jsx-no-bind": "xo-react", // generally disallowed but supports arrow functions (for convenience)
+  "react/jsx-no-duplicate-props": "xo-react", // wrong casing is typically a typo... should be checked by TS anyway...
+  "react/jsx-no-script-url": "kentdodds", // old style stuff... no place in modern FE development
+  "react/jsx-no-target-blank": "xo-react", // fine-tuned in XO to check spreads as well
+  "react/jsx-pascal-case": "kentdodds", // disabling all-caps like in XO as well
+  "react/no-string-refs": "xo-react", // fine-tuned in XO to prevent template strings as well
+  "react/no-unsafe": "kentdodds", // also disabled in strict-mode which should be standard for new developments
+  "react/no-unstable-nested-components": "kentdodds", // that's indeed looking like a common issue in code bases. Good find.
+  "react/no-unused-prop-types": "off", // TS mode: Do not care about prop-types
+  "react/prefer-es6-class": "off", // Seems to be about old classes to ES6 classes, but we prefer functional components anyway
+  "react/prop-types": "off", // TS mode: Do not care about prop-types
+  "react/require-default-props": "off", // TS mode: Do not care about prop-types
+  "react/sort-comp": "off", // Relevant for traditional classes only... edge case in todays code bases
+  "react/state-in-constructor": "xo-react", // Traditional classes, but pretty tricky if not detected... therefor enabled
+  "react/static-property-placement": "off", // Relevant for traditional classes only... edge case in todays code bases
+  "react/style-prop-object": "cra", // XO is right about FormattedNumber, but this might change in React-Intl as well... or one uses a different lib.
+
+  "unicorn/no-null": "xo-typescript" // Disabled as not relevant in TS projects
 }
 
 export const ruleBasedSourcePriorityOld: SourcePriorityTable = {
