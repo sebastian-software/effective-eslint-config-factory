aa779727f25b15301a3f340b95c567681618aa66
Sebastian Werner <s.werner@sebastian-software.de>
2022-07-13 09:47:01 +0200

    Improved integration of rules from Kent C Dodds WIP

--- src/loader.ts
+++ src/loader.ts
@@ -356,11 +356,6 @@ export function getKentDodds(): RuleLoaderReturn {
     ...jestPlugin.overrides[0].rules
   }
 
-  // Kent has a lot of rules explicitly disabled without
-  // any specific reason (e.g. TS replacement). By dropping these rules
-  // we reduce the conflicts number by nearly 100 rules through all rule sets.
-  removeDisabledRules(allRules)
-
   return {
     config: {},
 
--- src/rules.ts
+++ src/rules.ts
@@ -12,10 +12,26 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
     comment:
       "XO enforces camelCase properties, style heavy and controversial, but good thing to follow in 95% of code."
   },
+  "arrow-body-style": {
+    use: "xo",
+    comment: "It's actually good to keep parens - mostly more code accessibility."
+  },
+  "capitalized-comments": {
+    use: "kentdodds",
+    comment: "There are definitely more important things than formatting in comments."
+  },
+  "class-methods-use-this": {
+    use: "kentdodds",
+    comment: "The exception list for Airbnb shows that this might make sense in some cases. Better trust the individual developer."
+  },
   complexity: {
     use: "xo",
     comment:
       "Configured to 20 (default), Kent/XO only, feels like a good default."
+  },
+  "constructor-super": {
+    use: "eslint",
+    comment: "Obviuosly a good one. Typically an oversight."
   },
   "default-case": {
     use: "cra",
@@ -29,6 +45,14 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
   "func-names": {
     use: "kentdodds",
     comment: "Prefer better debug-ability when enabled"
+  },
+  "getter-return": {
+    use: "eslint",
+    comment: "Most commonly enabled to prevent accidential issues."
+  },
+  "grouped-accessor-pairs": {
+    use: "kentdodds",
+    comment: "Not used in most presets."
   },
   "max-depth": {
     use: "xo",
@@ -46,6 +70,10 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
     use: "xo",
     comment: "In Kent and XO, Kent defines a max=1 which is already the default"
   },
+  "no-async-promise-executor": {
+    use: "eslint",
+    comment: "Enabled for eslint recommended and XO. Makes sense to catch that."
+  },
   "new-cap": {
     use: "kentdodds",
     comment: "In Kent and XO, XO defines options which are already the default"
@@ -54,9 +82,65 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
     use: "eslint",
     comment: "Effectively identical in most presets."
   },
+  "no-const-assign": {
+    use: "kentdodds",
+    comment: "Easily solved by TypeScript and the execution runtime."
+  },
+  "no-dupe-args": {
+    use: "kentdodds",
+    comment: "Easily solved by TypeScript and the execution runtime."
+  },
+  "no-dupe-keys": {
+    use: "kentdodds",
+    comment: "Easily solved by TypeScript and the execution runtime."
+  },
+  "no-else-return": {
+    use: "kentdodds",
+    comment: "Mostly stylistic, no functional reason. Therefor a bad default choice in a preset."
+  },
   "no-empty": {
     use: "eslint",
     comment: "Effectively identical in most presets."
+  },
+  "no-eq-null": {
+    use: "kentdodds",
+    comment: "Generally okay. Type issues are also way better catched by TS anyway."
+  },
+  "no-extra-boolean-cast": {
+    use: "eslint",
+    comment: "Somewhat stylistic but as the cast is not required it's better to communicate this."
+  },
+  "no-func-assign": {
+    use: "eslint",
+    comment: "Disallowed by most presets for a reason: hard to track."
+  },
+  "no-implicit-coercion": {
+    use: "xo",
+    comment: "Accessibility wins over smartness. Easier to understand/read code is the result."
+  },
+  "no-import-assign": {
+    use: "eslint",
+    comment: "Good default. There is probably no reason to do so. Enabling prevents accidents here."
+  },
+  "no-misleading-character-class": {
+    use: "eslint",
+    comment: "Good default in recommended preset."
+  },
+  "no-new-symbol": {
+    use: "eslint",
+    comment: "Good default in recommended preset."
+  },
+  "no-obj-calls": {
+    use: "eslint",
+    comment: "Good default in recommended preset."
+  },
+  "no-promise-executor-return": {
+    use: "xo",
+    comment: "Seems reasonable. Is there a valid reason why this should be disabled?"
+  },
+  "no-prototype-builtins": {
+    use: "eslint",
+    comment: "Fair to keep this enabled even so nobody really extends Object.prototype anymore."
   },
   "no-labels": {
     use: "xo",
@@ -68,17 +152,45 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
     comment:
       "Richest collection in CRA, others only enable it, without any config."
   },
+  "no-restricted-properties": {
+    use: "kentdodds",
+    comment: "Very edge case stuff in CRA. Not worth keeping that rule alive for."
+  },
   "no-return-assign": {
     use: "kentdodds",
     comment: "Same in XO, otherwise unconfigured"
+  },
+  "no-return-await": {
+    use: "xo",
+    comment: "Informs about a small issue a lot of devs accidently do."
   },
   "no-self-assign": {
     use: "eslint",
     comment: "Effectively same value in all presets"
   },
+  "no-setter-return": {
+    use: "eslint",
+    comment: "Obvious issue. Good one. Might also be detected by TS though."
+  },
+  "no-this-before-super": {
+    use: "eslint",
+    comment: "Good one to prevent. Might lead to hard-to-track issues."
+  },
   "no-undef": {
     use: "xo",
     comment: "Typeof is enabled in XO only, which is a valid use case"
+  },
+  "no-underscore-dangle": {
+    use: "kentdodds",
+    comment: "Style only. Not worth for a presets default set."
+  },
+  "no-unreachable": {
+    use: "eslint",
+    comment: "Disallowed in most presets for a reason: reports accidental issues."
+  },
+  "no-use-before-define": {
+    use: "kentdodds",
+    comment: "Better let report these kind of issues by TS."
   },
   "no-unsafe-negation": {
     use: "eslint",
@@ -103,6 +215,10 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
     comment:
       "Off in most presets, controversial, might help to mark future tasks as a valid reason"
   },
+  "no-with": {
+    use: "eslint",
+    comment: "Disallowed by most presets. Hard to grasp in all consequences + tricky to optimize by a JIT."
+  },
   "object-shorthand": {
     use: "kentdodds",
     comment:
@@ -120,6 +236,26 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
   "prefer-const": {
     use: "xo",
     comment: "XO has fine tuned setting for destructing"
+  },
+  "prefer-destructuring": {
+    use: "kentdodds",
+    comment: "Mostly stylistic with not clear benefit in doing so. Drop it like Kent does."
+  },
+  "operator-assignment": {
+    use: "kentdodds",
+    comment: "Only stylistic and += is used that often in loops that it might hurt quite a bit."
+  },
+  "prefer-promise-reject-errors": {
+    use: "xo",
+    comment: "Good practise. Kind of suprising that this is not part of eslint/recommended."
+  },
+  "prefer-regex-literals": {
+    use: "",
+    comment: ""
+  },
+  "spaced-comment": {
+    use: "",
+    comment: ""
   },
   strict: {
     use: "kentdodds",
@@ -154,7 +290,6 @@ export const ruleBasedSourcePriority: SourcePriorityTable = {
   },
   "@typescript-eslint/no-this-alias": { use: "xo-typescript", comment: "" },
   "@typescript-eslint/no-throw-literal": { use: "xo-typescript", comment: "" },
-  // "@typescript-eslint/no-use-before-define":{use:"off", comment: "Not that relevant in todays world"},
   "@typescript-eslint/restrict-plus-operands": {
     use: "xo-typescript",
     comment: ""
