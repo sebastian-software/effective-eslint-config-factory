5af5fe6f602a3e507891713b18e79885369e5f9e
Sebastian Werner <s.werner@sebastian-software.de>
2022-07-26 12:59:38 +0200

    Hard-coding jest version into preset to prevent runtime requirement in importing app config.

--- src/override/testing.ts
+++ src/override/testing.ts
@@ -1,13 +1,18 @@
 import { Linter } from "eslint"
+import pkg from "jest/package.json"
 
 const override: Linter.ConfigOverride = {
   // Explicitly use `/src/*` as we might have e2e tests (Cypress/Playwright)
-  // in another root-level folder and do not like to risk compatibility.
+  // in another root-level folder e.g. `e2e` and do not like to risk compatibility.
   files: ["src/*.{spec,test}.{js,jsx,ts,tsx}"],
   plugins: ["jest", "testing-library"],
   env: {
-    jest: true,
     "jest/globals": true
+  },
+  settings: {
+    // Setting version here is smart as it prevent failing at the user project when
+    // no local jest version is installed (prevents probing).
+    version: pkg.version,
   }
 }
 
